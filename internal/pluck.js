da2d9eff29f26f292eef38a500515516eaa199ec
Lakshmikanth Vallampati <kanth.vallampati@gmail.com>
2022-10-05 10:46:41 +0530

    Update pluck.js

--- internal/pluck.js
+++ internal/pluck.js
@@ -1,9 +1,11 @@
 /**
- * Utility method to extract the given key from an array of objects
- * @param store source array of objects
+ * Utility method to extract the attribute from each model in the collection.
+ * @param store array of objects
  * @param key to extract
+ * @returns Returns the array of extracted values 
  *
  * @example
+ *
  * const list = [
  *   {name: 'jack', age: 14}, 
  *   {name: 'jill', age: 15},
@@ -13,4 +15,5 @@
  * pluck(list, 'age'); // [14, 15, 16]
  *
  */
+
 export const pluck = (store, key) => { return store.map(o => o[key]); }
