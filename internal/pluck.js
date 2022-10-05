055352e2b33f5d7ae11b63c9802a32925e623f3d
Lakshmikanth Vallampati <kanth.vallampati@gmail.com>
2022-10-05 10:41:19 +0530

    Update pluck.js with example

--- internal/pluck.js
+++ internal/pluck.js
@@ -1,6 +1,16 @@
 /**
- * Utility method to pluck a key from array of objects
- * @param array source array
- * @param key key to extract/pluck
+ * Utility method to extract the given key from an array of objects
+ * @param store source array of objects
+ * @param key to extract
+ *
+ * @example
+ * const list = [
+ *   {name: 'jack', age: 14}, 
+ *   {name: 'jill', age: 15},
+ *   {name: 'humpty', age: 16}
+ * ];
+ *
+ * pluck(list, 'age'); // [14, 15, 16]
+ *
  */
 export const pluck = (store, key) => { return store.map(o => o[key]); }
