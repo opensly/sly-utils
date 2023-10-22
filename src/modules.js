d1d370f89a8668c907d5558d303bb7bc4665f27f
Lakshmikanth Vallampati <kanth.vallampati@gmail.com>
2023-10-22 10:28:29 +0530

    feat: shuffle

--- src/modules.js
+++ src/modules.js
@@ -1,15 +1,17 @@
 import { camelToSnake } from './modules/camelToSnake.js';
 import { groupByArrayOfObjects } from './modules/groupByArrayOfObjects.js';
 import { pluck } from './modules/pluck.js';
+import { shuffle } from './modules/shuffle.js';
 import { snakeToCamel } from './modules/snakeToCamel.js';
 import { sortArrayOfObjects } from './modules/sortArrayOfObjects.js';
 import { transformKeys } from './modules/transformKeys.js';
 
 module.exports = {
   camelToSnake: camelToSnake,
-  snakeToCamel: snakeToCamel,
   groupByArrayOfObjects: groupByArrayOfObjects,
   pluck: pluck,
+  shuffle: shuffle,
+  snakeToCamel: snakeToCamel,
   sortArrayOfObjects: sortArrayOfObjects,
-  transformKeys: transformKeys,
+  transformKeys: transformKeys
 };
